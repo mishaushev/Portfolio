5f5cd1319d6d47d7e511c4cf69581644784f9daf
Misha Aushev <aushev97@gmail.com>
2021-08-24 21:31:57 +0200

    Created a footer Component

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer (){
+    return(
+        <div className="Footer">
+            <h1>Misha Aushev Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
